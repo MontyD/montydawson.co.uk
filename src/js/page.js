019852642c2ece2a4a7f027258445158c27d7fd2
montyD <montydawson@hotmail.co.uk>
2016-04-10 22:41:03 +0100

    beginning work page

--- src/js/page.js
+++ src/js/page.js
@@ -1,109 +1,113 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  // *************** //
-  // ** utilities ** //
-  // *************** //
-  function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-      var context = this,
-        args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  }
+    // *************** //
+    // ** utilities ** //
+    // *************** //
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
+    if (Blazy) {
+        var bLazy = new Blazy({
+            offset: 100
+        });
+    }
+    // ** Global object ** //
+    var landingPage = {
 
-  // ** Global object ** //
-  var landingPage = {
+        doc: document.documentElement,
 
-    doc: document.documentElement,
+        element: document.getElementsByTagName('body')[0],
 
-    element: document.getElementsByTagName('body')[0],
+        scrollPosition: 0,
 
-    scrollPosition: 0,
+        sections: document.getElementsByTagName('section'),
 
-    sections: document.getElementsByTagName('section'),
+        menu: {
 
-    menu: {
+            element: document.getElementById('menu'),
 
-      element: document.getElementById('menu'),
+            open: false,
 
-      open: false,
+            opener: {
 
-      opener: {
+                element: document.getElementById('hamburger'),
 
-        element: document.getElementById('hamburger'),
+                active: function() {
 
-        active: function() {
+                    return (this.element.className += ' active');
 
-          return (this.element.className += ' active');
+                },
+
+            },
+
+            triggerElements: document.getElementsByClassName('toggleMenu'),
+
+            toggle: function(e) {
+
+                var evt = e || window.event;
+                if (evt.preventDefault) {
+                    evt.preventDefault();
+                } else {
+                    evt.returnValue = false;
+                }
+                if (this.menu.open) {
+                    this.menu.element.className = "";
+                    this.menu.opener.element.className += " active";
+                } else {
+                    this.menu.element.className = "active";
+                    this.menu.opener.element.className = this.menu.opener.element.className.replace(' active', '');
+                }
+                return (this.menu.open = !this.menu.open);
+
+            },
 
         },
 
-      },
+        init: function() {
 
-      triggerElements: document.getElementsByClassName('toggleMenu'),
+            this.element.className = '';
 
-      toggle: function(e) {
+            for (var j = 0; j < this.sections.length; j++) {
+                this.sections[j].className += ' active';
+            }
 
-        var evt = e || window.event;
-        if (evt.preventDefault) {
-          evt.preventDefault();
-        } else {
-          evt.returnValue = false;
-        }
-        if (this.menu.open) {
-          this.menu.element.className = "";
-          this.menu.opener.element.className += " active";
-        } else {
-          this.menu.element.className = "active";
-          this.menu.opener.element.className = this.menu.opener.element.className.replace(' active', '');
-        }
-        return (this.menu.open = !this.menu.open);
-
-      },
-
-    },
-
-    init: function() {
-
-      this.element.className = '';
-
-      for (var j = 0; j <this.sections.length; j++) {
-        this.sections[j].className += ' active';
-      }
-
-      for (var i = 0; i < this.menu.triggerElements.length; i++) {
-        this.menu.triggerElements[i].onclick = (this.menu.toggle.bind(this));
-      }
+            for (var i = 0; i < this.menu.triggerElements.length; i++) {
+                this.menu.triggerElements[i].onclick = (this.menu.toggle.bind(this));
+            }
 
 
-    },
+        },
 
-    scroll: debounce(function() {
+        scroll: debounce(function() {
 
-      var bodyHeight = this.doc.clientHeight;
+            var bodyHeight = this.doc.clientHeight;
 
-      this.scrollPosition = (window.pageYOffset || this.doc.scrollTop) - (this.doc.clientTop || 0);
+            this.scrollPosition = (window.pageYOffset || this.doc.scrollTop) - (this.doc.clientTop || 0);
 
-    }, 20),
+        }, 20),
 
-  };
+    };
 
-  // //assign window level listeners
-  window.addEventListener('scroll', function() {
-    landingPage.scroll();
-  }, false);
-  //
-  window.addEventListener('load', function() {
-    landingPage.init();
-  }, false);
+    // //assign window level listeners
+    window.addEventListener('scroll', function() {
+        landingPage.scroll();
+    }, false);
+    //
+    window.addEventListener('load', function() {
+        landingPage.init();
+    }, false);
 
 })();
